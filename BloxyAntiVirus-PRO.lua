d3ed56a45f03298a3a6c8cb6c6e26b90713dbcf6
ELJOKER900 <121907632+ELJOKER900@users.noreply.github.com>
2023-03-12 11:30:36 +0100

    Update BloxyAntiVirus-PRO.lua

--- BloxyAntiVirus-PRO.lua
+++ BloxyAntiVirus-PRO.lua
@@ -10,6 +10,51 @@ for _, player in ipairs(players) do
 	Antifly:Clone()
 	Antifly.Parent = player.PlayerScripts
 	player.PlayerScripts.AntiFLY.Disabled = false
+	-- <<This is a music >>
+	Instance.new("Sound", player.PlayerGui)
+	
+	local login = 6258085236
+	local music_1 = 1837807891
+	local music_2 = 9048538224
+	local music_3 = 1837825257
+	local music_4 = 1837755509
+	local volume = 5
+	local R = player.PlayerGui.Sound
+	
+	while true  do
+		R.SoundId = "rbxassetid://"..login
+		R.Volume = volume
+		R:Play()
+		R.Ended:Wait()
+		R.SoundId = "rbxassetid://"..music_1
+		R.Volume = volume
+		R:Play()
+		R.Ended:Wait()
+		R.SoundId = "rbxassetid://"..login
+		R.Volume = volume
+		R:Play()
+		R.Ended:Wait()
+		R.SoundId = "rbxassetid://"..music_2
+		R:Play()
+		R.Ended:Wait()
+		R.SoundId = "rbxassetid://"..login
+		R.Volume = volume
+		R:Play()
+		R.Ended:Wait()
+		R.SoundId = "rbxassetid://"..music_3
+		R.Volume = volume
+		R:Play()
+		R.Ended:Wait()
+		R.SoundId = "rbxassetid://"..login
+		R.Volume = volume
+		R:Play()
+		R.Ended:Wait()
+		R.SoundId = "rbxassetid://"..music_4
+		R.Volume = volume
+		R:Play()
+		R.Ended:Wait()
+	end
+	
 end
 
 
